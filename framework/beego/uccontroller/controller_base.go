ee15271d9f887fb29d6d2e53b0a029466a9cade8
杜争斌 <847379962@qq.com>
2020-08-26 17:24:59 +0800

    [功能] beego controller 新增 解析参数接口
    Signed-off-by: 杜争斌 <zhengbin.du@quanshi.com>

--- framework/beego/uccontroller/controller_base.go
+++ framework/beego/uccontroller/controller_base.go
@@ -1,7 +1,10 @@
 package uccontroller
 
 import (
-	"github.com/astaxie/beego/context"
+	"bytes"
+	"context"
+	"encoding/json"
+	beegocontext "github.com/astaxie/beego/context"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -50,46 +53,80 @@ func (f *fakeResponseWriter) Write(b []byte) (int, error) {
 	return 0, nil
 }
 func (f *fakeResponseWriter) WriteHeader(n int) {}
-func (this *Controller) TestData(body string){
-	this.Ctx = &context.Context{
+func (ctr *Controller) TestData(body string){
+	ctr.Ctx = &beegocontext.Context{
 		Request: &http.Request{URL: &url.URL{Scheme: "http", Host: "localhost", Path: "/"}},
-		ResponseWriter:&context.Response{&fakeResponseWriter{},true,http.StatusOK,time.Nanosecond},
+		ResponseWriter:&beegocontext.Response{&fakeResponseWriter{},true,http.StatusOK,time.Nanosecond},
 	}
-	this.Ctx.Output = &context.BeegoOutput{Context: this.Ctx}
-	this.Ctx.Input = &context.BeegoInput{Context: &context.Context{Request: this.Ctx.Request}}
-	this.Ctx.Request.Header = http.Header{}
-	this.Ctx.Request.Header.Set("Content-Type","application/json")
-	this.Ctx.Input.RequestBody = []byte(body)
+	ctr.Ctx.Output = &beegocontext.BeegoOutput{Context: ctr.Ctx}
+	ctr.Ctx.Input = &beegocontext.BeegoInput{Context: &beegocontext.Context{Request: ctr.Ctx.Request}}
+	ctr.Ctx.Request.Header = http.Header{}
+	ctr.Ctx.Request.Header.Set("Content-Type","application/json")
+	ctr.Ctx.Input.RequestBody = []byte(body)
 
 }
+func (ctr *Controller) ParseRequest(ctx context.Context, v interface{}) (err error) {
+	log, _ := uclog.ExtractLogger(ctx)
+	contentType := ctr.Ctx.Input.Header("Content-Type")
+	body := ctr.Ctx.Input.RequestBody
+	query := ctr.Ctx.Request.URL.RawQuery
+	// 压缩请求参数中空白字符
+	var param string
+	dst := new(bytes.Buffer)
+	err = json.Compact(dst, body)
+	if err != nil {
+		param = strings.Replace(string(body), "\n", "", -1)
+	} else {
+		param = dst.String()
+	}
+	log.Log_Info("param:%s, query:%s", param, query)
+	if len(body) > 0 {
+		err = json.Unmarshal(body, v)
+		if err != nil {
+			// 有可能不是json格式的数据, 所以不能直接返回
+			if contentType == "application/json" {
+				return
+			} else {
+				values := ctr.Input()
+				if len(values) > 0 {
+					err = ctr.ParseForm(v)
+				}
+			}
+		}
+	}
+	if len(query) > 0 {
+		err = ctr.ParseForm(v)
+	}
+	return
+}
 // http_outputtype可以为.json(参见beego/mime.go，表示application/json)，或者"application/json; charset=utf-8"
-func (controller *Controller) InitContentType() {
+func (ctr *Controller) InitContentType() {
 	if beego.AppConfig != nil {
 		contentType := beego.AppConfig.DefaultString("http_outputtype", "")
 		if contentType != "" {
 			if strings.Contains(contentType, "/") {
-				controller.Ctx.Output.Header("Content-Type", contentType)
+				ctr.Ctx.Output.Header("Content-Type", contentType)
 			} else {
-				controller.Ctx.Output.ContentType(contentType)
+				ctr.Ctx.Output.ContentType(contentType)
 			}
 		}
 	}
 }
 
-func (controller *Controller) CheckClientSession() (err error) {
-	controller.UserID = ucutil.ToUint64V2(controller.Ctx.Input.Header("User-Id"))
-	if controller.UserID == 0 {
+func (ctr *Controller) CheckClientSession() (err error) {
+	ctr.UserID = ucutil.ToUint64V2(ctr.Ctx.Input.Header("User-Id"))
+	if ctr.UserID == 0 {
 		return ucerror.ErrorUserIDIsZero
 	}
-	controller.SessionID = ucutil.ToString(controller.Ctx.Input.Header("Session-Id"))
-	if controller.SessionID == "" {
+	ctr.SessionID = ucutil.ToString(ctr.Ctx.Input.Header("Session-Id"))
+	if ctr.SessionID == "" {
 		return ucerror.ErrorSessionIDIsEmpty
 	}
 	return nil
 }
 
-func (controller *Controller) GetUrlParam(key string) string {
-	for k, v := range controller.Input() {
+func (ctr *Controller) GetUrlParam(key string) string {
+	for k, v := range ctr.Input() {
 		if strings.ToLower(k) == strings.ToLower(key) {
 			if len(v) == 0 {
 				return ""
@@ -100,7 +137,7 @@ func (controller *Controller) GetUrlParam(key string) string {
 	return ""
 }
 
-func (controller *Controller) parseParams(v *reflect.Value, src string) bool {
+func (ctr *Controller) parseParams(v *reflect.Value, src string) bool {
 	switch v.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		if tempV, err := strconv.ParseInt(src, 10, 0); err == nil {
@@ -139,7 +176,7 @@ func (controller *Controller) parseParams(v *reflect.Value, src string) bool {
 	return false
 }
 
-func (controller *Controller) ParseRequestParams(dest interface{}) bool {
+func (ctr *Controller) ParseRequestParams(dest interface{}) bool {
 	if reflect.TypeOf(dest).Kind() != reflect.Ptr {
 		return false
 	}
@@ -158,35 +195,35 @@ func (controller *Controller) ParseRequestParams(dest interface{}) bool {
 			continue
 		}
 
-		paraStr := controller.GetUrlParam(key)
+		paraStr := ctr.GetUrlParam(key)
 		if key == "" {
 			continue
 		}
 
 		item := v.Field(i)
-		if !controller.parseParams(&item, paraStr) {
+		if !ctr.parseParams(&item, paraStr) {
 			return false
 		}
 	}
 	return true
 }
 
-func (controller *Controller) DoGetConfiguration() {
+func (ctr *Controller) DoGetConfiguration() {
 	data := map[string]interface{}{
 		"log_level": uclog.GetLogLevelDesc(),
 	}
-	controller.Rsp.Data = data
+	ctr.Rsp.Data = data
 }
 
-func (controller *Controller) DoSetConfiguration() {
-	loglevel := controller.GetUrlParam("log_level")
+func (ctr *Controller) DoSetConfiguration() {
+	loglevel := ctr.GetUrlParam("log_level")
 	if len(loglevel) == 0 {
-		controller.Rsp.Msg = "invalid log level"
+		ctr.Rsp.Msg = "invalid log level"
 		return
 	}
 	uclog.SetLogLevel(loglevel)
 	data := map[string]interface{}{
 		"log_level": uclog.GetLogLevelDesc(),
 	}
-	controller.Rsp.Data = data
+	ctr.Rsp.Data = data
 }
